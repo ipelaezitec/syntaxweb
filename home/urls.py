c72669f9b2983cce1f6f6f23b328ba8547d60d35
ipelaezitec <i.pelaez@itecriocuarto.org.ar>
2018-11-18 22:04:11 -0300

    animated css added,font-awesome-added, languages and report pannel items added

--- home/urls.py
+++ home/urls.py
@@ -6,6 +6,13 @@ urlpatterns = [
     path('syntaxposts/',views.syntaxposts,name='syntaxpost'),
     path('favorites/',views.favorites,name='favorites'),
     path('panel/', views.panel, name='panel'),
+    path('panel/langpulls',views.langpulls,name='langpulls'),
+    path('panel/languages',views.languages,name="langs"),
+    path('panel/languages/<int:id>', views.lang, name="lang"),
+    path('panel/langpulls/<int:id>',views.langpull,name="langpull"),
+    path('panel/langpulls/update/<int:id>',views.updateLanguage,name='updatelang'),
+    path('panel/reports/',views.reports,name='reports'),
+    path('panel/report/<int:id>',views.report,name='report'),
 
 ]
 #path('about/',views.about,name='web-about')
--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 
-from home.models import SyntaxPost,Language,Marker
+from home.models import SyntaxPost,Language,Marker,Sentence,Report
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
 
@@ -42,9 +42,79 @@ def favorites(request):
     }
     return render(request,'home/favorites.html',context)
 
+
+######################################## PANEL
+
 @staff_member_required
 def panel(request):
     usuario = request.user.id
 
     context = {}
     return render(request,'home/panel/panel.html',context)
+
+@staff_member_required
+def langpulls(request):
+    # esta para el orto, pero me cansé de renegar con esta query
+    langpulls = Language.objects.filter(active=False).order_by('-created')
+
+    context = {
+        'langpulls':langpulls,
+    }
+    return render(request, 'home/panel/langpulls.html',context)
+
+@staff_member_required
+def langpull(request,id):
+    syntaxposts = SyntaxPost.objects.filter(language_id=id)
+    #languageName = syntaxposts.first().language
+    #print(languageName)
+
+    context={
+        'syntaxposts': syntaxposts,
+
+    }
+    return render(request,'home/panel/langpull.html',context)
+
+@staff_member_required
+def updateLanguage(request,id):
+    lang = Language.objects.get(id=id)
+
+    lang.active = True
+    lang.save()
+    return redirect('langpulls')
+
+@staff_member_required
+def languages(request):
+    langs = Language.objects.filter(active=True).order_by('-created')
+    context = {
+        'langs':langs,
+    }
+    return render(request, 'home/panel/langs.html',context)
+
+@staff_member_required
+def lang(request,id):
+    syntaxposts = SyntaxPost.objects.filter(language_id=id)
+
+    context={
+        'syntaxposts': syntaxposts,
+
+    }
+    return render(request,'home/panel/lang.html',context)
+
+@staff_member_required
+def reports(request):
+    reports = Report.objects.filter(resolved='N').order_by('-created')
+    print (reports)
+    context = {
+        'reports': reports,
+    }
+    return render(request, 'home/panel/reports.html', context)
+
+@staff_member_required
+def report (request,id):
+    report = Report.objects.get(pk=id)
+    print (report)
+
+    context = {
+        'report':report,
+    }
+    return render(request, 'home/panel/report.html', context)
